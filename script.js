d58c792107172b4797f36f214241e7307fea1230
shuckin2017 <shuckin@mail.ru>
2020-10-17 21:14:26 +0300

    [WASP-1] Тестирование проекта

---
+++ script.js
@@ -0,0 +1,71 @@
+// let str = 'wefwefwefwefef';
+
+// alert(str[0]);
+// alert(str[2]);
+// alert(str[4]);
+// alert(str[0]);
+
+
+// console.log(60 * 60 * 24);
+
+// let arr = [1, 4, 3, 0, 4, 5, 4]
+
+
+// .filter(arr(element => !(element % 2)))
+// console.log(arr)
+
+
+// let arr = ['a', 'b', 'c', 'd', 'e'];
+
+// for (let i = 0; i <= arr.length - 1; i++) {
+// 	console.log(arr[i]);
+// }
+
+// let arr = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10];
+
+// for (let i = 0; i < arr.length; i++) {
+// 	if (arr[i] % 2 == 1) {
+// 		console.log(arr[i]);
+// 	}
+// }
+
+
+// let result = 0;
+// let arr = [1, 2, 3, 4, 5,6];
+
+// for (let i = 0; i < arr.length; i++) {
+// 	result += arr[i] ** 2;
+// }
+
+// console.log(result);
+
+// let arr = [0, 1, 2, 3, 4, 5 ],
+// 		max = arr[0];
+
+// function arrMeth(arr) {
+
+// 	for (let i = 0; i < arr.length; i++) {
+// 		if (arr[i] > max) {
+// 			max = arr[i];
+// 			console.log('True');
+// 		} else {
+// 			console.log('False');
+// 		}
+// 	}
+// }
+
+// arrMeth(arr);
+
+	// let arr = ['пн', 'вт', 'ср', 'чт', 'пт', 'сб', 'вс'];
+
+	// for(let i = 0; i < 7; i++) {
+
+
+	// 	console.log(arr[i]);
+	// }
+
+	let arr = [3, 4, 5]; 
+for (let i in arr){ 
+  console.log(i); 
+} 
+	
